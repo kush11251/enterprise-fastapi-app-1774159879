607176e9f2ad6813554bbc33d4c0ec36ea1404a9
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-22 11:41:34 +0530

    feat: integrate app/schemas.py

---
+++ app/schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    active: bool
